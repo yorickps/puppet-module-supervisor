8422465de07941766c3193093ed9e68ae53fe011
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-05 14:36:03 -0700

    Install supervisor via pip rather than system packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -160,7 +160,8 @@ class supervisor(
 
   if ! defined(Package[$supervisor::params::package]) {
     package { $supervisor::params::package:
-      ensure => $package_ensure,
+      ensure   => $package_ensure,
+      provider => 'pip'
     }
   }
 
