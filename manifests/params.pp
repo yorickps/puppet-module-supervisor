d7ded3471fb27c86afd588f560384f814b819930
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-05 14:11:01 -0700

    Change .ini to .conf, as God intended

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,14 +3,14 @@ class supervisor::params {
     'debian': {
       $conf_file      = '/etc/supervisor/supervisord.conf'
       $conf_dir       = '/etc/supervisor'
-      $conf_ext       = '.ini'
+      $conf_ext       = '.conf'
       $system_service = 'supervisor'
       $package        = 'supervisor'
     }
     'redhat': {
       $conf_file      = '/etc/supervisord.conf'
       $conf_dir       = '/etc/supervisord.d'
-      $conf_ext       = '.ini'
+      $conf_ext       = '.conf'
       $system_service = 'supervisord'
       $package        = 'supervisor'
     }
