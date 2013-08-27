033f88375871336e662cd4a4d26b9b5be58544c3
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-27 15:45:36 -0700

    Break supervisor plugins into submodule

---
+++ manifests/plugins/httpok.pp
@@ -0,0 +1,13 @@
+define supervisor::plugins::httpok (
+  $process,
+  $httpok_url,
+  $httpok_code = 200,
+) {
+
+  file { "${supervisor::params::conf_dir}/httpok.conf":
+    ensure  => $file_ensure,
+    content => template('supervisor/plugins/httpok.ini.erb'),
+    require => File[$conf_dir],
+    notify  => Service[$supervisor::params::system_service],
+  }
+}
