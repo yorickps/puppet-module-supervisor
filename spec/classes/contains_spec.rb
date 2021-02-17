1ceb70d927a537491e39dc9811e2b750a3932ca3
Yorick Poels <yorick.poels@kuleuven.be>
2021-02-17 21:21:16 +0100

    Add basic spec class test

---
+++ spec/classes/contains_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+it { is_expected.to compile.with_all_deps }
+describe 'Testing the dependancies between the classes' do
+	it { is_expected.to contain_class('ntp::install') }
+	it { is_expected.to contain_class('ntp::config') }
+	it { is_expected.to contain_class('ntp::service') }
+	it { is_expected.to contain_class('ntp::install').that_comes_before('Class[ntp::config]') }
+	it { is_expected.to contain_class('ntp::service').that_subscribes_to('Class[ntp::config]') }
+	it { is_expected.to contain_file('foo.rb').that_notifies('Service[ntp]') }
+end
