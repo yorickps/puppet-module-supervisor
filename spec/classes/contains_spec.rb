9ca7276a32a2264c6663838907ee8acdf973a9e8
Yorick Poels <yorick.poels@kuleuven.be>
2022-10-26 09:51:29 +0200

    Add spec tests and helpers

--- spec/classes/contains_spec.rb
+++ spec/classes/contains_spec.rb
@@ -3,13 +3,16 @@
 require 'spec_helper'
 
 describe 'supervisor' do
+  let(:facts) { { is_virtual: false } }
+
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge(super())
       end
 
-      context 'with default values' do
+      it { is_expected.to compile.with_all_deps }
+      describe 'Testing the dependancies between the classes' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('supervisor') }
         it { is_expected.to contain_class('supervisor::install').that_comes_before('Class[supervisor::config]') }
